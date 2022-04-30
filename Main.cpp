e9b60b82f58ec900fc3185c7129378d805ab957e
Blossom Ghuntla <bghuntla123@gmail.com>
2022-04-30 15:35:00 +0530

    red_color_window

--- Main.cpp
+++ Main.cpp
@@ -3,158 +3,53 @@
 #include<GL/glew.h>
 #include<iostream>
 #include<string>
-#include<fstream>
-constexpr auto numVAOs = 1; // will process during compile time
 using namespace std;
 
-// Logs
-void printShaderLog(GLuint shader) {
-	int len = 0;
-	int chWrittn = 0;
-	char* log;
-	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
-	if (len > 0) {
-		log= (char*)malloc(len);
-		glGetShaderInfoLog(shader, len, &chWrittn, log);
-		cout << "Shader Info Log:" << log << endl;
-		free(log);
-	}
-}
-
-void printProgramLog(int prog) {
-	int len = 0;
-	int chWrittn = 0;
-	char* log;
-	glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
-	if (len > 0) {
-		log = (char*)malloc(len);
-		glGetProgramInfoLog(prog, len, &chWrittn, log);
-		cout << "Program Info Log: " << log << endl;
-		free(log);
-	}
-}
-
-bool checkOpenGLError() {
-	bool foundError = false;
-	int glErr = glGetError();
-	while (glErr != GL_NO_ERROR) {
-		cout << "glError: " << glErr << endl;
-		foundError = true;
-		glErr = glGetError();
-	}
-	return foundError;
-}
-
-GLuint renderingProgram;
-GLuint vao[numVAOs];
-
-string readShaderSource(const char* filePath) {
-	string content;
-	ifstream fileStream(filePath, ios::in);
-	string line = "";
-
-	while (!fileStream.eof()) {
-		getline(fileStream, line);
-		content.append(line + "\n");
-	}
-	fileStream.close();
-	return content;
-}
-
-GLuint createShaderProgram() {
-	GLint vertCompiled;
-	GLint fragCompiled;
-	GLint linked;
-
-	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
-	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-	string vertShaderStr = readShaderSource("D:\\All_Projs\\c++_Proj\\OGLPractise\\vertShader.glsl");
-	string fragShaderStr = readShaderSource("D:\\All_Projs\\c++_Proj\\OGLPractise\\fragShader.glsl");
-
-	const char* vertShaderSrc = vertShaderStr.c_str();
-	const char* fragShaderSrc = fragShaderStr.c_str();
-
-	glShaderSource(vShader, 1, &vertShaderSrc, NULL);
-	glShaderSource(fShader, 1, &fragShaderSrc, NULL);
-
-	glCompileShader(vShader);
-	checkOpenGLError();
-	glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
-	if (vertCompiled != 1) {
-		cout << "vertex compilation failed" << endl;
-		printShaderLog(vShader);
-	}
-	
-	glCompileShader(fShader);
-	checkOpenGLError();
-	glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);
-	if (fragCompiled != 1) {
-		cout << "frag compilation failed" << endl;
-		printShaderLog(fShader);
-	}
-
-	GLuint vfProgram = glCreateProgram();
-	glAttachShader(vfProgram, vShader);
-	glAttachShader(vfProgram, fShader);
-	glLinkProgram(vfProgram);
-	checkOpenGLError();
-	glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
-	if (linked != 1) {
-		cout << "linking failed" << endl;
-		printProgramLog(vfProgram);
-	}
-	return vfProgram;
-}
-
 void init(GLFWwindow* window) {
-	renderingProgram = createShaderProgram();
-	glGenVertexArrays(numVAOs, vao);
-	glBindVertexArray(vao[0]);
+
 }
 
-// https://stackoverflow.com/questions/61946603/glpointsize-fails-to-shrink-a-point
-// point values
-GLfloat pointSize = 100.0f;
-GLfloat increment = 2.0f;
-
- //Color values
 void display(GLFWwindow* window, double currentTime) {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glUseProgram(renderingProgram);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
 
 int main(void) {
-	if (!glfwInit()) {
-		//Initializing GLFW
-		exit(EXIT_FAILURE);
-	}
+    //Initialize GLFW
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
+    }
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	GLFWwindow* window = glfwCreateWindow(640, 480, "color window", NULL, NULL);
-	if (!window) {
-		exit(EXIT_FAILURE);
-		glfwTerminate();
-	}
-	glfwMakeContextCurrent(window);
-	
-	if (glewInit() != GLEW_OK) {
-		// Initializing GL
-		exit(EXIT_FAILURE);
-	}
-	glfwSwapInterval(1);
-	
-	init(window);
-	while (!glfwWindowShouldClose(window)) {
-		display(window, glfwGetTime());
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+    //OpenGL version
+    glfwWindowHint(GLFW_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_VERSION_MAJOR, 6);
+    //Window creation
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Practise", NULL, NULL);
+    //Window does not match
+    if (!window) {
+        exit(EXIT_FAILURE);
+        glfwTerminate();
+    }
+    //Select current context
+    glfwMakeContextCurrent(window);
 
-	// Destroying the window
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	exit(EXIT_SUCCESS);
+    //Initialize GLEW
+    if (glewInit() != GLEW_OK) {
+        exit(EXIT_FAILURE);
+    }
+    //avoid screen tear useful for High-motion-conten-graphics(vsync)
+    glfwSwapInterval(1);
+
+    init(window);
+    while (!glfwWindowShouldClose(window)) {
+        display(window, glfwGetTime());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    //Destory GLFW window
+    glfwDestroyWindow(window);
+    //Terminate GLFW
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
