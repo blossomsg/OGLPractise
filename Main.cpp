cd7de484c65e1d26b3575c78969f9e858ea29aed
Blossom Ghuntla <bghuntla123@gmail.com>
2023-04-06 18:48:04 +0530

    yellow window

--- Main.cpp
+++ Main.cpp
@@ -1,52 +1,40 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <GL/glew.h>
 #include <iostream>
 
-int add_values(int value_x, int value_y) {
-	int value = value_x + value_y;
-	return value;
+//using namespace std;
+
+//void init(GLFWwindow* window){
+//
+//}
+
+void display(GLFWwindow* window, double currentTime) {
+	glClearColor(1.0, 1.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
 }
 
-int sub_values(int value_x, int value_y) {
-	int value = value_x - value_y;
-	return value;
-}
+int main(void){
+	if (!glfwInit()) { 
+		exit(EXIT_FAILURE); 
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "COLOR_WIN", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK) {
+		exit(EXIT_FAILURE);
+	}
+	glfwSwapInterval(1);
+	
+	//init(window);
 
-int mul_values(int value_x, int value_y) {
-	int value = value_x * value_y;
-	return value;
-}
-
-int div_values(int value_x, int value_y) {
-	int value = value_x/ value_y;
-	return value;
-}
-
-bool greater_values(int value_x, int value_y) {
-	return(value_x > value_y);
-}
-
-bool smaller_values(int value_x, int value_y) {
-	return(value_x < value_y);
-}
-
-int diameter_of_circle(int radius) {
-	return radius * 2;
-}
-
-int metres_to_centimetres(int value) {
-	return value * 100;
-}
-
-int main() {
-	int value_x = 32;
-	int value_y = 68;
-	int radius = 3;
-	int m = 5;
-	std::cout << add_values(value_x, value_y) << std::endl;
-	std::cout << sub_values(value_x, value_y) << std::endl;
-	std::cout << mul_values(value_x, value_y) << std::endl;
-	std::cout << div_values(value_y, value_x) << std::endl;
-	std::cout << greater_values(value_y, value_x) << std::endl;
-	std::cout << smaller_values(value_y, value_x) << std::endl;
-	std::cout << diameter_of_circle(radius) << std::endl;
-	std::cout << metres_to_centimetres(m) << std::endl;
+	while (!glfwWindowShouldClose(window)) {
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
 }
